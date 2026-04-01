34a31d1a2b5475a5775310079488ba6b9b117e3c
AntoniaDnv <antonia.d.neva@gmail.com>
2026-04-01 21:42:02 +0300

    Bug fix, deleted unused files, logic fix

--- HouseRentingSystem/Controllers/HouseController.cs
+++ HouseRentingSystem/Controllers/HouseController.cs
@@ -6,46 +6,42 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystem.Controllers
 {
-	public class HouseController : Controller
-	{
-		private List<HouseViewModel> houses = new List<HouseViewModel>()
-		{
-			new HouseViewModel()
-			{   Id = 1,
-				Name = "Beach house",
-				Address = "Miami, Florida",
-				ImageUrl = @"https://i.ytimg.com/vi/Y4gLQQrKf1E/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDF-Ss8WtuTN_zmjUqGsuJa5rpJIw"
-			},
-			new HouseViewModel()
-			{
-				Id=2,
-				Name = "Mountain house",
-				Address = "Rila Mountain, Bulgaria",
-				ImageUrl = @"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8LkVGvrwA_10W1Jizdesx9DmZeydC1wh5IA&s"
-			},
-			new HouseViewModel()
-			{
-				Id=3,
-				Name = "Urban House",
-				Address = "Lulin, Sofia, Bulgaria",
-				ImageUrl = @"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO3CW-ztFIRbSl2yNpP7LtSBEkZ92ZOaPeDg&s"
-			}
-		};
+    public class HouseController : Controller
+    {
         private readonly HouseRentingDbContext context;
+
         public HouseController(HouseRentingDbContext context)
         {
             this.context = context;
         }
+
         public async Task<IActionResult> AllHouses()
         {
-            var houses = await context.Houses.ToListAsync();
-            return View(houses);
+            var housesModel = await context.Houses
+                .Select(h => new HouseViewModel()
+                {
+                    Id = h.Id,
+                    Name = h.Title, 
+                    Address = h.Address,
+                    ImageUrl = h.ImageUrl
+                })
+                .ToListAsync();
+
+            return View(housesModel);
         }
 
         public async Task<IActionResult> HouseById(int id)
         {
             var house = await context.Houses
-                .FirstOrDefaultAsync(h => h.Id == id);
+                .Where(h => h.Id == id)
+                .Select(h => new HouseViewModel()
+                {
+                    Id = h.Id,
+                    Name = h.Title,
+                    Address = h.Address,
+                    ImageUrl = h.ImageUrl
+                })
+                .FirstOrDefaultAsync();
 
             if (house == null)
             {
@@ -69,7 +65,7 @@ namespace HouseRentingSystem.Controllers
                 return View(model);
             }
 
-            var house = new House()
+            var houseEntity = new House()
             {
                 Title = model.Title,
                 Description = model.Description,
@@ -77,16 +73,10 @@ namespace HouseRentingSystem.Controllers
                 ImageUrl = model.ImageUrl
             };
 
-            await context.Houses.AddAsync(house);
+            await context.Houses.AddAsync(houseEntity);
             await context.SaveChangesAsync();
 
             return RedirectToAction(nameof(AllHouses));
         }
-
-        public IActionResult Details(int id)
-		{
-			return View(houses.FirstOrDefault(h => h.Id == id));
-		}
-
-	}
+    }
 }
